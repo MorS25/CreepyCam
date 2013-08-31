11254e6695d2557820d706cd8158bd7f6e199cb1
Siebes <ryansiebert@live.co.uk>
2013-08-31 21:56:29 +1000

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include "util.h"
+#include <iostream>
+#include <string>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+using namespace cv;
+using namespace std;
+
+int main ()
+{
+	/*  USER CAN EDIT THESE VARIABLES */
+
+	int delay = 500;
+	char* default_dir = "/home/pi/CreepyCam/images/";
+	
+	/* ############################# */
+	/* #### DO NOT TOUCH BELOW ##### */
+
+
+	bool init = false;
+	CvCapture* creepyCam = NULL;
+	cout << "Welcome to CreepyCam" << endl;
+	cout << "Pull the latest version on" << endl;
+	cout << "https://github.com/Jamble/CreepyCam" << endl;
+
+	/* INITILIZE */
+	init = initilizeCreepy(default_dir);
+	if(init == false){
+		cout << "Something went bad initilizing" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	snapAndSave(default_dir, "test.jpg");
+	exit(EXIT_SUCCESS);
+}
