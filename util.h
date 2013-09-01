1c23ad1a6af1665426ee1fa549125f39d5209300
Siebes <ryansiebert@live.co.uk>
2013-09-01 21:44:39 +1000

    added more function prototypes

--- util.h
+++ util.h
@@ -8,7 +8,11 @@ bool initilizeCreepy(char* dir);
 bool checkDir(char* dir);
 bool checkCam();
 void snapAndSave(char* dir, char* fileName);
+cv::Mat takePicture();
 cv::Mat createDifferentialImage(cv::Mat img1, cv::Mat img2);
 cv::Mat xORImage(cv::Mat img1, cv::Mat img2);
+bool checkForMotion(cv::Mat xorimg, int threshold);
+void saveImg(char* dir, cv::Mat img);
+void motionThread(char* dir, int threshold);
 
 #endif
