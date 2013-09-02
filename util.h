5320c83e314497e6d984c0a3c729cfe523b57d86
Siebes <ryansiebert@live.co.uk>
2013-09-02 18:09:14 +1000

    test working. motion not

--- util.h
+++ util.h
@@ -12,7 +12,7 @@ cv::Mat takePicture();
 cv::Mat createDifferentialImage(cv::Mat img1, cv::Mat img2);
 cv::Mat xORImage(cv::Mat img1, cv::Mat img2);
 bool checkForMotion(cv::Mat xorimg, int threshold);
-void saveImg(char* dir, cv::Mat img);
+void saveImg(char* fileName, char* dir, cv::Mat img);
 void motionThread(char* dir, int threshold);
 
 #endif
